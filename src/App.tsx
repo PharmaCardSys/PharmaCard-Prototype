53fc416685f87235f5af38d2b3ff1e1941a4e9df
PharmaCardSys <pharmacardcapstone@gmail.com>
2026-01-08 11:47:01 +0800

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+    return (
+        <>
+            <div className="bg-blue-500 text-white p-4 rounded-lg shadow-md">
+                <h1 className="text-xl font-bold">Pharma Card App</h1>
+            </div>
+        </>
+    );
+}
+
+export default App;
